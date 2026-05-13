9effaa9a5df8bc2454c972547383e477ebf50e39
yurakh-maxym-dfg <maksaaasssdfg@gmail.com>
2026-05-13 16:36:20 +0300

    task 3-5 done

--- js/lesson-2.js
+++ js/lesson-2.js
@@ -31,3 +31,51 @@
 // checkLogin(logins);
 
 // Task 3
+
+
+// function calculateAverage(...args) {
+//     let sum = 0;
+    
+//     for (const num of args) {
+//         if (typeof num !== 'number' || Number.isNaN(num)) {
+//             return 'That not a number!';
+//         }
+//         sum += num;
+//     }
+//     const averg = sum / args.length;
+//     return averg;
+// }
+
+// console.log(calculateAvarage(1, 2, 3, 4, 5, 6, 7, 8));
+
+// Task 4
+
+// const someArr = [22, 11, 34, 5, 12, 13, 14, 15];
+
+// function summNumArray(array) {
+//     const sumArr = [];
+//     for (let i = 0; i < array.length - 1; i++){
+//         sumArr.push(array[i] + array[i + 1]);
+//     }
+//     return sumArr;
+// }
+
+// console.log(summNumArray(someArr));
+
+// Task 5
+const numbers = [2, 5, 35, 56, 12, 24, 7, 80, 3];
+
+function findSmallestNumber(numbers) {
+    if (!Array.isArray(numbers)) {
+        return 'That not an array!';
+    }
+    let res = numbers[0];
+    for (let i = 0; i < numbers.length; i++){
+        if (numbers[i] < res) {
+            res = numbers[i];
+        }
+    }
+    return res;
+}
+
+console.log(findSmallestNumber(numbers));
